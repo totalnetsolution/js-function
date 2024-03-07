f3057fff4c0ea7802dc02331b411c0c6fa585507
muhammad-danish <totalnetsolution2@gmail.com>
2024-03-08 02:09:03 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,292 @@
+// function sum(num1 , num2){
+//     console.log("sum of 2 numbers is" , num1 + num2)
+// }
+// sum(12  , 14)
+
+// var lastname = khan;
+
+// function greetuser(firstname){
+
+// }
+
+// function showString(){
+//     console.log("Hello World")
+//     return "Abdullah"
+// }
+// var myfunct = showString()
+// console.log(myfunct);
+
+
+// function showString(){
+//     console.log("Hello World")
+//     return "Muhammad Danish"
+// }
+// var myfuncti = showString()
+// console.log(myfuncti);
+
+// function showString(number1 , number2){
+//     console.log(number1 + number2)
+//     return "Abdullah"
+// }
+// var myfunc = showString(12 , 15)
+// console.log(myfunc);
+
+
+// function parcentagecal
+// (marks1 , marks2 , marks3 ,marks4 ){
+// return obtainmarks / totalmarks * 100
+// }
+// var danishparcentage = parcentagecal(10,  20 , 30 , 40);
+// console.log(danishparcentage);
+
+
+
+// console.log('hello world');
+
+// alert()
+// prompt()
+
+
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+
+
+
+
+
+
+
+
+
+
+// function greetUser (){   
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// console.log('hello world');
+// }
+
+// greetUser()
+// greetUser()
+
+
+
+
+
+
+
+// function greetUser(){
+//     console.log('hello world');
+//     console.log('hello world');
+//     console.log('hello world');
+// }
+// greetUser()
+// greetUser()
+
+
+
+
+
+
+
+
+
+
+
+//undefined
+//null
+
+// var username ;
+// console.log(username)
+
+
+
+
+
+
+
+
+
+
+// function greetUser(username , lastname , age){
+//     console.log('hello ' + username + lastname + age);
+// }
+
+// greetUser('abdullah' , 'khan' , 20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sum(num1 , num2){
+//     console.log('sum of two number ===>' , num1 + num2)
+// }
+// sum(19 , 20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var lastname = 'khan'
+
+// function greetUser (){
+//     var username = 'abdullah'
+//     console.log(username + lastname);
+// }
+
+// console.log(username)
+// console.log(lastname)
+
+// greetUser()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function showString(){
+//     console.log('hello world')
+//     return 'abdullah'
+// }
+// // showString()
+// var myFunc = showString()
+// console.log(myFunc);
+
+
+
+
+
+
+// function percentageCal (marks1 , marks2 , marks3 , marks4){
+//     var totalMarks = 400;
+//     var obtainedMarks = marks1 + marks2 + marks3 + marks4
+//     return obtainedMarks / totalMarks * 100
+
+// }
+
+// var usmanPercentage = percentageCal(10 , 50 , 90 ,60);
+
+// console.log(usmanPercentage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var head = document.querySelector('.head');
+// console.log(head.innerHTML);
+// head.innerHTML = 'changed through js'
+// console.log(head.innerHTML);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var head = document.querySelector('.head');
+var paragraph = document.querySelector("#para");
+console.log(paragraph.innerHTML)
+
+function changeText(){
+
+    if(paragraph.innerHTML === 'changed through js' && head.innerHTML === 'changed through js'){
+        paragraph.innerHTML = 'Here you can know how Function work in javascript';
+        head.innerHTML = 'Hello Function'
+    }else{
+        paragraph.innerHTML = 'changed through js'
+        head.innerHTML = 'changed through js'
+    }
+}
+
+
+
